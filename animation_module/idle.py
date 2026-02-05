44d06b9503cd653f9d5a8e08fe91d6fdc58995e4
vibhanshu <vibhanshu28july@gmail.com>
2026-02-05 17:33:57 +0530

    amimation_module_v1

--- animation_module/idle.py
+++ animation_module/idle.py
@@ -1,10 +1,5 @@
 import cv2
-from fastapi import FastAPI, Request
-from fastapi.responses import StreamingResponse, HTMLResponse
-from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 # Replace 'your_video.mp4' with the path to your file
 VIDEO_PATH = 'animations/idle.mp4'
 camera = cv2.VideoCapture(VIDEO_PATH)
@@ -26,15 +21,3 @@ def gen_frames():
         
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-
-@app.get("/video_feed")
-async def video_feed():
-    return StreamingResponse(gen_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
-
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-     return templates.TemplateResponse("index.html", {"request": request}) 
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
+import animation_module.talking as am_talking
+import animation_module.idle as am_idlle
+import animation_module.bye as am_bye
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/video_feed")
+async def video_feed():
+    return StreamingResponse(am_bye.gen_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+     return templates.TemplateResponse("index.html", {"request": request}) 
