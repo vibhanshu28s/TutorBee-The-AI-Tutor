43107fc9906d5bf2c6747a00c9d5ff30fe877a4e
vibhanshu <vibhanshu28july@gmail.com>
2026-02-06 11:07:54 +0530

    voice_input/voice_assistant

---
+++ voice_input/voice_assistant.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+
+
+def listen_to_voice():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        while True:
+            print("Listening...")
+            audio = r.listen(source)
+            try:
+                command = r.recognize_google(audio).lower()
+                print(f"You said: {command}")
+                if "exit" in command:
+                    break
+            except sr.UnknownValueError:
+                print("Could not understand audio")
+            except sr.RequestError:
+                print("API unavailable")
